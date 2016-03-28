19bd765715b90ad7c1108ee0e0a82a8ba9e84953
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2016-03-28 23:55:11 +0300

    Changed page title. Changed a user's avatar id.

--- app/src/users/UserService.js
+++ app/src/users/UserService.js
@@ -41,7 +41,7 @@
       },
       {
         name: 'Gani Ferrer',
-        avatar: 'svg-6',
+        avatar: 'svg-10',
         content: "Lebowski ipsum yeah? What do you think happens when you get rad? You turn in your library card? Get a new driver's license? Stop being awesome? Dolor sit amet, consectetur adipiscing elit praesent ac magna justo pellentesque ac lectus. You don't go out and make a living dressed like that in the middle of a weekday. Quis elit blandit fringilla a ut turpis praesent felis ligula, malesuada suscipit malesuada."
       }
     ];
