cc185a48499fc47a228e3d3c3995e310f625e8d6
Oleksiy Rudenko <orudenko@bigmir.net>
2016-04-03 00:15:07 +0300

    mv App.js AppModule.js since App.js staging failed

---
+++ app/src/app/AppModule.js
@@ -0,0 +1,11 @@
+/**
+ * Created by oleksiy.rudenko on 02/04/2016.
+ */
+(function(){
+    'use strict';
+
+    // Prepare the 'app' module for subsequent registration of controllers and delegates.
+    angular.module('app', [ 'ngMaterial' ]);
+
+
+})();
