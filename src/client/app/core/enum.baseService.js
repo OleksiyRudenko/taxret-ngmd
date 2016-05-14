0ca4880519a23b2793d98c02b132ec894dbdeb22
Oleksiy Rudenko <orudenko@bigmir.net>
2016-05-14 21:06:37 +0300

    enumBase: cache SelectOptions results, add BuildProperties method

--- src/client/app/core/enum.baseService.js
+++ src/client/app/core/enum.baseService.js
@@ -5,24 +5,36 @@
     .factory('enumBase', EnumBaseService);
   
   EnumBaseService.$inject=[
-    // '$q',
+    '$log',
     // '$rootScope'
   ];
 
   /**
    * @name EnumBaseService
    * @desc Provides public interface for enumerations. Used by enum.*.service
-   * @ param $q
-   * @returns enumeration
+   * @param $log
+   * @returns interface
    * @constructor
    */
   function EnumBaseService(
-    // $q
+    $log
     // $rootScope
   ){
     var enumBase = {
+      // properties : ...,  // maps principal properties' values to anything else, e.g. human readable descriptions or
+                            // those multilingual;
+                            // auto-constructed unless defined by child object;
+                            // auto-construction converts principal_property:value => .properties.value={descr:"principal_property"}
+      // derivatives : {},      // populated by helper functions based on main properties or .properties
+                            // normally, contains private secondary structures
       _constructor : function () {
+        this.derivatives = { BuildProperties : BuildProperties, };
         this.properties = this.properties || {}; // create unless exists
+        if (Object.keys(this.properties).length == 0) {
+          // build .properties
+          $log.debug("Building .properties:");
+          this.derivatives.BuildProperties.call(this);
+        }
         this.selectOptions = SelectOptions;
       }
     };
@@ -36,16 +48,33 @@
     /*
      * @name SelectOptions
      * @desc Generates array to generate <SELECT> options
+     * @param forceRebuild optional
      * @returns Array of objects { value: , label: }
      */
-    function SelectOptions() {
-      var props = this.properties;
-      var options = [];
-      for (var attr in props) {
-        if (props.hasOwnProperty(attr))
-          options.push({value: attr, label: props[attr].descr});
+    function SelectOptions(forceRebuild) {
+      if (typeof this.derivatives.selectOptions == 'undefined' || forceRebuild) {
+        // populate .derivatives.selectOptions
+        var props = this.properties;
+        this.derivatives.selectOptions = [];
+        for (var attr in props) {
+          if (props.hasOwnProperty(attr))
+            this.derivatives.selectOptions.push({value: attr, label: props[attr].descr});
+        }
       }
-      return options;
+      return this.derivatives['selectOptions'];
+    }
+
+    /*
+     * @name BuildProperties
+     * @desc Generates .properties if empty or inexistent
+     */
+    function BuildProperties() {
+      var self = this;
+      for (var attr in self) {
+        if (self.hasOwnProperty(attr) && attr!='properties' && attr!='derivatives')
+          self.properties[self[attr]] = { descr : attr };
+      }
+      $log.debug(this.properties.toSource());
     }
   }
 })();
