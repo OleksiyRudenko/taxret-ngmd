dc301e62e133f7ccdccb102c9cd1ac6d06219e41
Oleksiy Rudenko <orudenko@bigmir.net>
2016-05-15 21:21:51 +0300

    Stub FetchData* methods

--- src/client/app/core/network.dataFetcher.service.js
+++ src/client/app/core/network.dataFetcher.service.js
@@ -26,21 +26,75 @@
     $log
   ) {
     var service = {
-      http_           : $http,
-      queryTemplate_  : null,       // e.g. 'https://www.google.com/finance/historical?q={q}&authuser=0&output=csv'
-      FetchData       : FetchData,
+      http_             : $http,
+      queryTemplate_    : './content/data/{q}.json',
+                          // remote e.g. 'https://www.google.com/finance/historical?q={q}&authuser=0&output=csv'
+      files             : ['admin','mockDeclarants',], // each json file contains array of objects
+      FetchData         : FetchData,
+      FetchDataFromLocal  : FetchDataFromLocal,
+      FetchDataFromLocalForItem_ : FetchDataFromLocalForItem_,
+      FetchDataFromRemote : FetchDataFromRemote,
     };
 
     return service;
     
     /**
      * @name  FetchData
-     * @desc  Fetches data over http or from local file
+     * @desc  Fetches data over http or, if  from local file
      * @param {boolean} [fromFile]
      */
-    function FetchData(fromFile) {
-
+    function FetchData(remote) {
+      remote = remote || false;
+      return (remote)
+        ? this.FetchDataFromRemote()
+        : this.FetchDataFromLocal();
+      }
+  
+    /**
+     * @name FetchDataFromLocal
+     */
+    function FetchDataFromLocal() {
+      var promises = this.files.map(this.FetchDataFromLocalForItem_, this);
+      return Promise.all(promises)
+        .then(
+          function(arrayPerFile) {
+            var result = [];
+            arrayPerFile.forEach(function(rows) {
+              result.push.apply(result,rows);
+            });
+            return result;
+          }
+        );
     }
+
+    /**
+     * @name  FetchDataFromLocalForItem_
+     * @param {string} filename
+     * @private
+     */
+    function FetchDataFromLocalForItem_(filename) {
+      var url = this.queryTemplate_.replace('{q}',filename);
+      return this.http_.get(url)
+        .then(
+          function(result) {
+            return result.data.map(function(obj){
+              // add some extra data if required
+              $log.debug("network.dataFetcher.service:FetchDataFromLocalForItem_ :: "
+                + filename + ".obj = "
+                + angular.toJson(obj,true));
+              return obj;
+            });
+          }
+        );
+    }
+      
+      /**
+       * @name FetchDataFromRemote
+       */  
+      function FetchDataFromRemote() {
+    
+      }
+    
 
   }
 
