2b6b4a8a38fcd9b1c910e192a82e826fd4dc919f
Oleksiy Rudenko <orudenko@bigmir.net>
2016-05-15 16:25:36 +0300

    Stub DataNetworkFetcher

---
+++ src/client/app/core/data.NetworkFetcher.service.js
@@ -0,0 +1,6 @@
+/*
+    Base path: https://github.com/google/lovefield/tree/master/codelabs/stock_analyzer
+    For service prototype see: src/common/data_fetcher.js
+    For use see: src/common/fetcher_service.js
+
+ */
