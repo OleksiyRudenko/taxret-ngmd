b6f490c3067e82372a1a48378766d0b751884d08
Oleksiy Rudenko <oleksiy.rudenko@gmail.com>
2016-06-21 15:34:05 +0300

    Declare module ngLocalDB

---
+++ src/client/app/core/localDB/ngLocalDB.module.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Oleksiy Rudenko on 21/06/2016.
+ */
+(function(){
+  'use strict';
+
+// Prepare the 'app' module for subsequent registration of configuration, controllers and delegates
+  angular
+    .module('ngLocalDB', [
+      'kutomer.ng-lovefield',
+    ]);
+
+})();
